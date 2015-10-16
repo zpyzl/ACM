4d04426a142f567de3142f6743836ff640a265f1
zpyzl <zpyzl@qq.com>
2015-10-17 01:44:44 +0800

    3

---
+++ src/poj/dp/p1015/Test.java
@@ -0,0 +1,36 @@
+package poj.dp.p1015;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		Main.f(genInput());
+	}
+	
+	public static InputStream genInput(){
+		StringBuilder input = new StringBuilder();
+		
+		Random rand = new Random();
+		
+		for(int r = 0; r < 5; r++){//round
+			//header
+			int n = 150+rand.nextInt(20);
+			int m = 15+rand.nextInt(4);
+			input.append(n+" "+m+"\n");
+			
+			for( int l = 0; l < n;l++){
+				int d = rand.nextInt(20);
+				int p = rand.nextInt(20);
+				input.append(d+" "+p+"\n");
+			}
+		}
+		input.append("0 0");
+		
+		InputStream in =   new  ByteArrayInputStream(input.toString().getBytes());
+		return in;
+	}
+}
