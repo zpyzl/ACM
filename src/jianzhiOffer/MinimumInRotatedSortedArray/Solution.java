0e1db2a9e87c58069c7271c278cc6b305ca5e714
zpyzl <zpyzl@qq.com>
2017-03-30 01:34:16 +0800

    1/4 version

--- src/jianzhiOffer/MinimumInRotatedSortedArray/Solution.java
+++ src/jianzhiOffer/MinimumInRotatedSortedArray/Solution.java
@@ -7,44 +7,46 @@ public class Solution {
     }
     
     private Integer insert3Stubs(int[] nums, int begin, int end){
-    	if( nums.length == 1)
-    		return nums[0];
-    	if( nums.length == 2)
-    		return nums[1];
-    	if( nums.length == 3){
-    		if( nums[0] > nums[1])
-    			return nums[1];
-    		else if( nums[1] > nums[2])
-    			return nums[2];
+    	int len = end - begin;
+    	
+    	if( len == 1)
+    		return nums[begin];
+    	if( len == 2)
+    		return nums[begin+1];
+    	if( len == 3){
+    		if( nums[begin] > nums[begin+1])
+    			return nums[begin+1];
+    		else if( nums[begin+1] > nums[begin+2])
+    			return nums[begin+2];
     		else 
-    			return nums[0];
+    			return nums[begin];
     	}
     	
     	InsertRes res1, res2, res3;
-    	res1 = insertStub(nums, nums.length/2/2) ;
+    	res1 = insertStub(nums, len/2/2) ;
     	if( res1.min != null )
     		return res1.min;
-    	res2 = insertStub(nums,nums.length/2) ;
+    	res2 = insertStub(nums,len/2) ;
     	if( res2.min != null )
     		return res2.min;
-    	res3 = insertStub(nums,nums.length*3/4);
+    	res3 = insertStub(nums,len*3/4);
 		if( res3.min != null )
     		return res3.min;
 		
 		if( res1.insertPoint < res2.insertPoint && res2.insertPoint < res3.insertPoint){
-			Integer r1 = insert3Stubs( nums, 0, nums.length/2/2 );
+			Integer r1 = insert3Stubs( nums, begin, len/2/2 );
 			if( r1 == null )
-				return insert3Stubs( nums, nums.length*3/4, nums.length );
+				return insert3Stubs( nums, len*3/4, len );
 			else 
 				return r1; 
 		}
 		if( res1.insertPoint < res2.insertPoint && res2.insertPoint > res3.insertPoint
 				&& res1.insertPoint > res3.insertPoint )
-			return insert3Stubs( nums, nums.length/2 , nums.length*3/4 );
+			return insert3Stubs( nums, len/2 , len*3/4 );
 		
 		if( res1.insertPoint > res2.insertPoint && res2.insertPoint < res3.insertPoint
 				&& res1.insertPoint > res3.insertPoint )
-			return insert3Stubs( nums, nums.length/2/2 , nums.length/2 );
+			return insert3Stubs( nums, len/2/2 , len/2 );
 		
 		return null;
     }
