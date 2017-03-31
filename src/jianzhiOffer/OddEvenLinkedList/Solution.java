6d1422442ec8d982995d380a91df418b995effc2
zpyzl <zpyzl@qq.com>
2017-03-31 21:19:51 +0800

    print utils

--- src/jianzhiOffer/OddEvenLinkedList/Solution.java
+++ src/jianzhiOffer/OddEvenLinkedList/Solution.java
@@ -1,10 +1,9 @@
 package jianzhiOffer.OddEvenLinkedList;
 
+import common.ListNode;
+import common.PrintUtils;
 import org.junit.Test;
 
-/**
- * Created by zpy on 2017/3/30.
- */
 public class Solution {
 
     public ListNode oddEvenList(ListNode head) {
@@ -23,34 +22,7 @@ public class Solution {
         }
         odd.next = evenHead;
         return head;
-
-        /*if( head == null || head.next == null || head.next.next == null ){
-            return head;
-        }
-
-        //init even list
-        ListNode firstEven = head.next;
-        ListNode evenTail = firstEven;
-
-        ListNode cursor = head.next.next;
-        head.next = cursor;
-
-        while( cursor.next != null && cursor.next.next != null  ){
-            evenTail.next = cursor.next;
-            evenTail = evenTail.next;
-
-            cursor.next = cursor.next.next;
-            cursor = cursor.next;
-        }
-        evenTail.next = null;
-        if( cursor.next != null && cursor.next.next == null ){
-            evenTail.next = cursor.next;
-        }
-        cursor.next = firstEven;
-
-        return head;*/
     }
-
     @Test
     public void test1(){
         ListNode h = new ListNode(1);
@@ -59,7 +31,7 @@ public class Solution {
             n.next = new ListNode(i);
             n = n.next;
         }
-        print( oddEvenList(h));
+        PrintUtils.print( oddEvenList(h));
 
     }
 
@@ -71,15 +43,10 @@ public class Solution {
             n.next = new ListNode(i);
             n = n.next;
         }
-        print( oddEvenList(h));
+        PrintUtils.print( oddEvenList(h));
+
 
     }
 
-    private void print(ListNode h){
-        ListNode n = h;
-        while(n != null ) {
-            System.out.print(n.val);
-            n = n.next;
-        }
-    }
+
 }
