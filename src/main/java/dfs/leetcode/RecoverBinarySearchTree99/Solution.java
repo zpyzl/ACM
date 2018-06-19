63b3d4725affa1d30c77c78bb320ce41b0e8cbda
zhaopengyang <zhaopengyang@dianwoba.com>
2018-06-19 17:20:59 +0800

    long after drop

--- src/main/java/dfs/leetcode/RecoverBinarySearchTree99/Solution.java
+++ src/main/java/dfs/leetcode/RecoverBinarySearchTree99/Solution.java
@@ -6,36 +6,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Morris Traversal to achieve constant space cost
  * Created by zhaopengyang on 2018/5/7.
  */
 public class Solution {
-    Integer first ;
-    Integer second ;
+    int firstToSwap ;
+    int secondToSwap;
+
     List<Integer> toSwap = new ArrayList<Integer>();
 
     public void recoverTree(TreeNode root) {
-        traverse(root.left);
+
+        TreeNode node = root, parent = null;
+        //go to left most
+        while( root.left != null ) {
+            //thread here?
+            parent = node;
+            node = node.left;
+        }
+        //node is leftChild
+        forLeftChild(node, parent);
+        visit(node);
+        visit(parent);
+        //new iterate
+        node = parent.right;
 
 
-        traverse(root.right);
+        //visit left
+
+        //visit node
     }
-
-    private void traverse(TreeNode node) {
-        traverse(node.left);
-
-        if( first == null && second == null ){
-            first = node.val;
-        }else if( first != null && second == null ){
-            second = node.val;
-        }else {
-            first = second;
-            second = node.val;
-            if( first > second ){
-                toSwap.add();
-            }
+    void forLeftChild(TreeNode leftChild, TreeNode parent){
+        //if child is leaf, left child point to its parent, right child points to parent
+        if( leftChild != null && leftChild.right == null ) {
+            leftChild.right = parent;
         }
 
-        traverse(node.right);
+    }
+    void forRightChild(TreeNode rightChild, TreeNode grandParent){
+        if( rightChild != null && rightChild.right == null ){
+            rightChild.right = grandParent;
+        }
+        visit(rightChild);
+    }
+
+    private void traverse(TreeNode node, TreeNode parent) {
+        //TODO node parent not null
+
+
     }
 
     private void visit(TreeNode node) {
