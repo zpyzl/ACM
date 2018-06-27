784f38a0a195026f3096a85bf35085c5d9c264d3
zhaopengyang <zhaopengyang@dianwoba.com>
2018-06-27 11:45:58 +0800

    long after drop

---
+++ src/main/java/string/longestPalindromic5/Solution.java
@@ -0,0 +1,10 @@
+package string.longestPalindromic5;
+
+/**
+ * Created by zhaopengyang on 2018/6/19.
+ */
+public class Solution {
+    //for a solution p for list(n), the char before list x
+    // switch a new char y
+    //  next to p: if x == y and x != null, solution (x,p,x)
+}
